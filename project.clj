f4074f3e8a59169e21abc8a28af8efb3e43b6ca7
David Groulx <david@sandbendersoftware.com>
2018-11-16 12:57:26 -0500

    Remove EPL

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject fusoya "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [ring/ring-core "1.7.1"]
                  [ring/ring-jetty-adapter "1.7.1"]
